d3aa7a87d8dcc6699e21e66ec50bf118caf5edff
Hardik Vora <hardik7304@gmail.com>
2022-12-14 16:16:48 -0500

    Download button for chart added

--- frontend/src/components/story/pie.js
+++ frontend/src/components/story/pie.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
+import { Button } from '@mui/material';
+import { saveAs } from 'file-saver';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
@@ -24,9 +26,22 @@ const PieComponent = ({ like, dislike }) => {
         ],
     };
 
+    const saveChart = () => {
+        const storyChart = document.getElementById('storyChart');
+        storyChart.toBlob(function (blob) {
+            saveAs(blob, "chart.png")
+        });
+    };
+
     return (
         <div style={{ width: '20%', margin: 'auto' }}>
-            <Pie data={data} />
+            <Pie id='storyChart' data={data} />
+            <Button
+                variant='outlined'
+                size='small'
+                sx={{ marginTop: '3%' }}
+                onClick={saveChart}
+            >Download</Button>
         </div>
     );
 };
